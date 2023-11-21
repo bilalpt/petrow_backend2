4c190d0d261e44429af75e29de2424660732afe5
bilalpt <bilalpt572@gmail.com>
2023-11-21 22:32:33 +0530

    last commit

--- petboarding/views.py
+++ petboarding/views.py
@@ -1,27 +1,27 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
-# from .serilizers import *
-# from .models import *
-# from rest_framework.views import APIView
-# from rest_framework.response import Response
-# # from rest_framework.generics import CreateAPIView
-# from rest_framework_simplejwt.views import TokenObtainPairView
-# from django.contrib.sites.shortcuts import get_current_site
-# from django.template.loader import render_to_string
-# from django.contrib.auth.tokens import default_token_generator
-# from django.core.mail import EmailMessage
-# from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-# from django.shortcuts import HttpResponseRedirect
-# from django.contrib.auth import authenticate
-# from django.utils.http import urlsafe_base64_encode
-# from django.utils.encoding import force_bytes
+from .serilizers import *
+from .models import *
+from rest_framework.views import APIView
+from rest_framework.response import Response
+# from rest_framework.generics import CreateAPIView
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.shortcuts import HttpResponseRedirect
+from django.contrib.auth import authenticate
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
 
-# from rest_framework.generics import RetrieveUpdateDestroyAPIView
-# from rest_framework import status
-# from rest_framework.decorators import api_view
-# from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework_simplejwt.tokens import RefreshToken
+from decouple import config
 # from decouple import config
-# # from decouple import config
 
 
 
